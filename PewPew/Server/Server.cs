8d1fa30a90dc8d60bafda310420d5c188fef30ad
Amir Moualem <amoualem@gmail.com>
2013-04-24 10:58:52 +0300

    Committing new solution

---
+++ PewPew/Server/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PewPew.Server
+{
+    class Server
+    {
+        private Socket _serverSocket;
+
+        public Server()
+        {
+            _serverSocket = null;
+        }
+
+    }
+}
