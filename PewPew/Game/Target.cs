947f8ef980ad591df2d7c0a6b95a408120e337fb
Shay Malchi <shaymalchi@gmail.com>
2013-04-26 00:30:17 +0300

    sort enemy strings

--- PewPew/Game/Target.cs
+++ PewPew/Game/Target.cs
@@ -18,10 +18,10 @@ namespace PewPew.Game
         public enum TargetName { Action1, Action2, Action3, Action4 };
         public static Dictionary<TargetName, TargetType> EnemyTypes = new Dictionary<TargetName, TargetType>()
         { 
-            { TargetName.Action1, new TargetType { inputText = "water,ice,wind", color = System.Windows.Media.Brushes.ForestGreen, fileName = "comb1.png" } },
-            { TargetName.Action2, new TargetType { inputText = "stone,fire,magic", color = System.Windows.Media.Brushes.Yellow, fileName = "comb2.png"  } },
-            { TargetName.Action3, new TargetType { inputText = "ice,wind,magic", color = System.Windows.Media.Brushes.Purple, fileName = "comb3.png"  } },
-            { TargetName.Action4, new TargetType { inputText = "fire,wind,ice", color = System.Windows.Media.Brushes.Teal, fileName = "comb4.png" } }
+            { TargetName.Action1, new TargetType { inputText = "ice,water,wind", color = System.Windows.Media.Brushes.ForestGreen, fileName = "comb1.png" } },
+            { TargetName.Action2, new TargetType { inputText = "fire,magic,stone", color = System.Windows.Media.Brushes.Yellow, fileName = "comb2.png"  } },
+            { TargetName.Action3, new TargetType { inputText = "ice,magic,wind", color = System.Windows.Media.Brushes.Purple, fileName = "comb3.png"  } },
+            { TargetName.Action4, new TargetType { inputText = "ice,fire,wind", color = System.Windows.Media.Brushes.Teal, fileName = "comb4.png" } }
         };
 
         public static TargetType EnemyTypeByInputText(String inputText)
@@ -30,3 +30,4 @@ namespace PewPew.Game
         }
     }
 }
+
